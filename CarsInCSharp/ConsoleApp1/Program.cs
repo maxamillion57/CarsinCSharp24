815f449d9898d7de64eb0f721e3efda5f494045e
maxamillion57 <maxpayne9789@gmail.com>
2024-01-16 20:21:30 -0500

    First Version
    Car related objects and menu (1 error)

---
+++ CarsInCSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary1;
+
+int userInput = 0;
+List<Car> cars = new List<Car>();
+while (userInput != 4)
+{
+    Console.WriteLine("Welcome to Max's Dealership");
+    Console.WriteLine("");
+    Console.WriteLine("-----------------");
+    Console.WriteLine("1. Add a new car");
+    Console.WriteLine("2. List all cars");
+    Console.WriteLine("3. Remove a car");
+    Console.WriteLine("4. Exit");
+    string usersInput = "";
+    try
+    {
+
+
+        usersInput = Console.ReadLine();
+        userInput = int.Parse(usersInput);
+    }
+    catch (Exception ex)
+    {
+    }
+    switch (userInput)
+    {
+        case 0:
+            break;
+        case 1:
+            Console.WriteLine("Add a new car");
+            string make, model, color;
+            int year;
+            Console.WriteLine("Enter the Make:");
+            make = Console.ReadLine();
+            Console.WriteLine("Enter the Model:");
+            model = Console.ReadLine();
+            Console.WriteLine("Enter the Color:");
+            color = Console.ReadLine();
+            Console.WriteLine("Enter the Year:");
+            year = int.Parse(Console.ReadLine());
+            Car newCar = new Car(make, model, year, color);
+            cars.Add(newCar);
+            break;
+        case 2:
+            Console.WriteLine("List all cars");
+            foreach (var car in cars)
+            {
+                car.DisplayInfo();
+            }
+            break;
+        case 3:
+            Console.WriteLine("Remove a car");
+            break;
+    }
+}
